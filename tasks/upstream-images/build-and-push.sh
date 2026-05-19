16f6167215f9ffdb016c92e0b3373aeb7c861f24
Evelyn Tanigawa Murasaki <emurasak@redhat.com>
2026-05-19 19:48:58 -0300

    make install

--- tasks/upstream-images/build-and-push.sh
+++ tasks/upstream-images/build-and-push.sh
@@ -30,6 +30,8 @@ build_and_push() {
     sed -i.bak 's/-f Dockerfile\.[a-zA-Z_-]*/-f Dockerfile/g' Makefile && rm -f Makefile.bak
     sed -i.bak 's/npm ci --ignore-scripts/npm ci/g' Makefile && rm -f Makefile.bak
 
+    make install
+
     if [[ "$repo" == "monitoring-plugin" ]]; then
         make podman-cross-build-push VERSION="$version" PLUGIN_NAME=monitoring-console-plugin
     else
